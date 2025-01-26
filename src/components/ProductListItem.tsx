417b928137b2bfb7eaa995469bd703f1b25b4bdf
Atharv <atharvakumar0@gmail.com>
2025-01-26 22:02:00 +0530

    Added Products List

--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -9,7 +9,11 @@ const defaultPizzaImage = "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/f
 export default function ProductListItem({ product }: ProductListItemProps) {
   return (
     <View style={styles.container}>
-      <Image source={{uri: product.image || defaultPizzaImage}} style={styles.image} />
+      <Image 
+        source={{uri: product.image || defaultPizzaImage}} 
+        style={styles.image}
+        resizeMode="contain"
+      />
       <Text style={styles.title}>{product.name}</Text>
       <Text style={styles.price}>${product.price}</Text>
     </View>
@@ -21,7 +25,8 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     padding: 10,
     borderRadius: 20,
-
+    flex: 1,
+    maxWidth: "50%",
   },
   image: {
     width: "100%",
